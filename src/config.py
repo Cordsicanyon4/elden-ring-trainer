c67fef23718b3755c66d27551bffed1007b1dafc
Cordsicanyon4 <arianhaazn595@gmail.com>
2026-06-05 16:41:08 +0200

    Add src/config.py

---
+++ src/config.py
@@ -0,0 +1,10 @@
+STAT_BOOST = 20
+ITEM_MODIFICATION = True
+QUICK_SAVE = True
+SCRIPT_PATH = ""
+
+# Example configuration
+# STAT_BOOST = 10
+# ITEM_MODIFICATION = False
+# QUICK_SAVE = False
+# SCRIPT_PATH = "path/to/script.py"
